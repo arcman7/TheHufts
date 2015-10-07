c443ca3b8d9dd14d21e832a75f7e0d051d10b118
Andrew Ryan Carpenter <andrewryancarpenter77@gmail.com>
2015-10-06 18:33:17 -0700

    save working point

--- App/app.js
+++ App/app.js
@@ -26,7 +26,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-// app.use("/public/javascript", express.static(__dirname + "/public/javascript"));
+
 app.use('/', index);
 app.use('/users', users);
 
