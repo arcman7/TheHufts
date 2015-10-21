d38aecfcb1b9d7bed69d14ff1826600b5f7fe917
Andrew Ryan Carpenter <andrewryancarpenter77@gmail.com>
2015-10-20 20:15:10 -0700

    update package.json

--- App/public/javascript/loginFront.js
+++ App/public/javascript/loginFront.js
@@ -1,3 +1,78 @@
-function loginFormSubmit(){
-  $()
+function navboxListener(){
+  //#login
+  //#register
+  $('.cancel').on("click",function(){
+    event.preventDefault();
+    if ( $(".active-form") ){
+      $(".active-form") .removeClass("active-form");
+    }
+  });
+
+  $("#login").on("click",function(){
+    event.preventDefault();
+    $(".active-form").removeClass("active-form");
+    $(".login-form").addClass("active-form");
+    console.log("login clicked");
+  });
+
+  $("#register").on("click",function(event){
+    event.preventDefault();
+    $(".active-form").removeClass("active-form");
+    $(".register-form").addClass("active-form");
+    console.log("register clicked");
+  });
 }
+
+function encrypt(string,key){
+  var encrypted = CryptoJS.AES.encrypt(string,key).toString();
+  return encrypted;
+}
+
+function decrypt(string,key){
+  var decrypted = CryptoJS.AES.decrypt(string, key);
+  return decrypted.toString(CryptoJS.enc.Utf8);
+}
+
+
+
+function formSubmitListener(){
+  var confirmation = "TheHufts";
+  var gateKeeperURL = "U2FsdGVkX1/TgCOk5cMhFLg/9AnetMh2IYRno+wQGk78aKwkRS39/rop2c/Cm3SpOtrz2UQHSNgZOie01+kZQg==";
+
+  $(".login-form").on("submit",function(event){
+    event.preventDefault();
+    console.log("form submitted");
+    var username = $('.login-form input[name="username"]').val();
+    var password = $('.login-form input[name="password"]').val();
+    var data = { username: username, password: password, confirmation: confirmation};
+    ajaxLoginRouter(data,decrypt(gateKeeperURL,confirmation));
+  });
+
+}
+
+function ajaxLoginRouter(data,url){
+  data = JSON.stringify(data);
+  $.ajax({
+          url: url,
+          type: "get"
+      }).
+  done(function(response){
+    var key = response;
+    data = encrypt(data, key);
+    var loginURL = "U2FsdGVkX18QPZdMyeLqAkP32qAdsz5D1W4iRNrujhzK7jsbsxqL4Fur1NrXDNfk34cDezQ52BUvIP7WBT71sg==";
+    var confirmation = encrypt("TheHufts",key);
+    $.ajax({
+        url: decrypt(loginURL,"TheHufts"),
+        type: "post",
+        data: {data: data}
+    })
+    .done(function(response){ console.log(response); } );
+  });
+}
+
+
+
+$(document).on("ready",function(){
+  navboxListener();
+  formSubmitListener();
+})
