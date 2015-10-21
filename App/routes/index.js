ae0b483b49269f3efe1574525c1e49e6e159a512
Arturo Perez <VrturoPerez@gmail.com>
2015-10-20 19:40:49 -0700

    add login and signup view

--- App/routes/index.js
+++ App/routes/index.js
@@ -6,12 +6,15 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'TheHufts' });
 });
 
-router.get('/homepage', function(req, res, next) {
-  res.render('homepage', { title: 'TheHufts' });
-});
-
 router.get('/landingPage', function(req, res, next) {
   res.render('landingPage', { title: 'TheHufts' });
 });
 
+router.get('/logIn', function(req, res, next) {
+  res.render('logIn', { title: 'TheHufts' });
+});
+
+router.get('/signUp', function(req, res, next) {
+  res.render('signUp', { title: 'TheHufts' });
+});
 module.exports = router;
