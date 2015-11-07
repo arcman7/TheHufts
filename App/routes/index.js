367a24ef680a71909d277d3fdf26920da60d32a1
Arturo Perez <VrturoPerez@gmail.com>
2015-11-06 18:43:33 -0800

    add logo to imgs and navbar

--- App/routes/index.js
+++ App/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'TheHufts' });
+  res.render('landingPage', { title: 'TheHufts' });
 });
 
 router.get('/landingPage', function(req, res, next) {
