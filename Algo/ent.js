98f98c2a01ecd3af46208f97d3bf6d5b9cc8386d
Andrew Ryan Carpenter <andrewryancarpenter77@gmail.com>
2016-01-10 21:45:39 -0800

    syntax issues

--- Algo/ent.js
+++ Algo/ent.js
@@ -1,4 +1,4 @@
-function ent(array,options=undefined){
+function ent(data_set,options){
   var buy     = [];
   var sell    = [];
   var signals = {};
@@ -9,7 +9,7 @@ function ent(array,options=undefined){
 
   //helper functions
   function slide_time_window(data_set,window){
-    var window = [];
+    var new_window = [];
     for(var i=1; i< window.length; i++){
       new_window.push(window[i]);
     }
@@ -21,8 +21,9 @@ function ent(array,options=undefined){
   function initalize_window(size, data_set){
     var window = [];
     for(var i = 0; i < size; i++){
-      window.push_back(data_set[i]);
+      window.push(data_set[i]);
     }
+    //console.log(window);
     return window;
   }
 
@@ -30,45 +31,54 @@ function ent(array,options=undefined){
     //var pCount, nCount;
     var pU,pN;
     pCount = nCount = 0; //uses global variables pCount & nCount
-    var entropy;
+    var entropy,total;
     for(var i = 0; i < window.length-1; i++){
-      if(window[i].price < window[i+1].price){
+      if(window[i][1] < window[i+1][1]){
         pCount += 1;
       }
-      if(window[i].price > window[i+1].price){
+      if(window[i][1] > window[i+1][1]){
         nCount += 1;
       }
-      pU = pCount*1.0 / window.length*1.0;
-      pN = nCount*1.0 / window.length*1.0;
+
     }
+    total = pCount + nCount;
+    pU = pCount*1.0 / total*1.0;//window.length*1.0;
+    pN = nCount*1.0 / total*1.0;//window.length*1.0;
     entropy = -1*pU*Math.log(pU) -1*pN*Math.log(pN);
     return entropy;
   }
 
-  function mapped_entropy(double& entropy){
-    double entropy_max = 0.301029995664;
+  function mapped_entropy(entropy){
+    var entropy_max = Math.log(0.5);//base e --> 0.301029995664;
+    //console.log(entropy);
     return (1-Math.pow(entropy/entropy_max,2) );
   }
  //helper functions END
 
-  window = initalize_window(14, data_set);
-
+  window = initalize_window(16, data_set);
+  var selltracker = false;
   while( index < data_set.length ){
       entr = entropy(window);
       mapped_ent = mapped_entropy(entr);
+
+      //console.log(mapped_ent);
       //0.7 is an arbitrary constant, and in general will be a parameter to be varied and optimized
       //mapped_entropy uses powers of 2 (symetric with undervalued and overvalued stock state), and thus we must make sure nCount > pCount is true
-      if( mapped_ent > 0.7 && nCount > pCount){
+      if( mapped_ent > 0.6 && nCount > pCount){
         buy.push(data_set[index]);
+        selltracker = true;
+        //console.log(selltracker);
       }
-      var last_index = sell.length -1;
-      if(last_index > -1){
-        if( data_set[index].price > buy[last_index].price){
+      var last_index = buy.length -1;
+      if(last_index > -1){ //checking if there any buy signals to make sell signals from
+        console.log(data_set[index][1] > buy[last_index][1], selltracker);
+        if( data_set[index][1] > buy[last_index][1] && selltracker){
           sell.push(data_set[index]);
+          selltracker = false;
         }
       }
 
-      window = slide_time_window(array, window);
+      window = slide_time_window(data_set, window);
   }//end while
 
   signals["buy"]  = buy;
---
+++ App/public/javascript/_globalVariable.js
@@ -0,0 +1,3 @@
+  var locationArray = window.location.href.split('/');
+  var protocol      = locationArray[0];
+  var domain        = locationArray[2];
